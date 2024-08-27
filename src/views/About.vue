ba47367624756c916880d080125e3ecab6eec34a
Hong Shik Branden Kim <kimploo@gmail.com>
2024-08-27 10:18:23 +0900

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div>
+    <h1>AboutData</h1>
+    <p>Welcome to AboutData</p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div>
+    <h1>HomeData</h1>
+    <p>Welcome to HomePage</p>
+  </div>
+</template>
+
+<style scoped></style>
